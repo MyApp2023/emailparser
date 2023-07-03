657428fbc7e402683523ddf044bd4fafac34a2d4
MyApp2023 <138391486+MyApp2023@users.noreply.github.com>
2023-07-03 18:55:17 -0400

    Update App.py

--- App.py
+++ App.py
@@ -98,22 +98,20 @@ search_engine_id = config.get("SEARCH_ENGINE_ID", "")
 # Main program
 st.title("Email Parser")
 
-# Prompt for password input
-password_key = get_unique_key()
-password = st.text_input("Enter password:", key=password_key)
-password = password[:30]  # Limit password length to 30 characters
-
-# Sign in button
-sign_in_button_key = get_unique_key()
-sign_in = st.button("Sign In", key=sign_in_button_key)
-
 # Track sign-in status using session state
 if 'signed_in' not in st.session_state:
     st.session_state.signed_in = False
 
 # Authenticate user
-if sign_in and password and verify_password(password):
-    st.session_state.signed_in = True
+if not st.session_state.signed_in:
+    # Prompt for password input
+    password_key = get_unique_key()
+    password = st.text_input("Enter password:", key=password_key)
+    password = password[:30]  # Limit password length to 30 characters
+
+    # Sign in logic
+    if password and verify_password(password):
+        st.session_state.signed_in = True
 
 if st.session_state.signed_in:
     st.success("Authentication successful!")
@@ -163,7 +161,7 @@ if st.session_state.signed_in:
 else:
     if is_user_locked():
         st.error("Too many failed login attempts. Please try again later.")
-    elif sign_in and password:
+    elif password:
         st.warning("Authentication failed. Please try again.")
         lock_user()
 
