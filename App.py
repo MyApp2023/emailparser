e58b0269472b1b1615833b2d1d9d5055887faf47
MyApp2023 <138391486+MyApp2023@users.noreply.github.com>
2023-07-03 20:06:36 -0400

    Update App.py

--- App.py
+++ App.py
@@ -123,39 +123,43 @@ if st.session_state.signed_in:
     search_query_key = get_unique_key()
     search_query = st.text_input("Enter the search string:", key=search_query_key)
 
-    api_choice_key = get_unique_key()
-    api_choice = st.selectbox(
-        "Select to use Google Businesses or Google Search:",
-        ('Google Businesses', 'Google Search'),
-        key=api_choice_key
-    )
+    if search_query:
+        # Hide password input and sign-in button
+        st.session_state.signed_in = True
 
-    num_results_key = get_unique_key()
-    num_results = st.number_input("How many URLs do you want to get?", min_value=1, max_value=MAX_URLS, step=1, value=1, key=num_results_key)
+        api_choice_key = get_unique_key()
+        api_choice = st.selectbox(
+            "Select to use Google Businesses or Google Search:",
+            ('Google Businesses', 'Google Search'),
+            key=api_choice_key
+        )
 
-    # Search and extract e-mails button
-    search_emails_button_key = get_unique_key()
-    search_emails = st.button("Search and extract e-mails", key=search_emails_button_key)
+        num_results_key = get_unique_key()
+        num_results = st.number_input("How many URLs do you want to get?", min_value=1, max_value=MAX_URLS, step=1, value=1, key=num_results_key)
 
-    if search_query and api_choice and num_results and search_emails:
-        if api_choice == 'Google Businesses' and google_maps_api_key:
-            st.info("Fetching URLs and e-mails using Google Places API...")
-            urls = get_place_urls(search_query, num_results, google_maps_api_key)
-            email_addresses = find_email_addresses(urls)
-            for i, (url, email_list) in enumerate(email_addresses.items(), start=1):
-                st.write(f"\n{i}. {url}\n")
-                for email in email_list:
-                    st.write(f"- {email}")
-        elif api_choice == 'Google Search' and google_search_api_key and search_engine_id:
-            st.info("Fetching URLs and e-mails using Google Custom Search API...")
-            urls = get_search_results(search_query, num_results, google_search_api_key, search_engine_id)
-            email_addresses = find_email_addresses(urls)
-            for i, (url, email_list) in enumerate(email_addresses.items(), start=1):
-                st.write(f"\n{i}. {url}\n")
-                for email in email_list:
-                    st.write(f"- {email}")
-        else:
-            st.error("Missing API key or search engine ID. Please check the configuration.")
+        # Search and extract e-mails button
+        search_emails_button_key = get_unique_key()
+        search_emails = st.button("Search and extract e-mails", key=search_emails_button_key)
+
+        if search_emails:
+            if api_choice == 'Google Businesses' and google_maps_api_key:
+                st.info("Fetching URLs and e-mails using Google Places API...")
+                urls = get_place_urls(search_query, num_results, google_maps_api_key)
+                email_addresses = find_email_addresses(urls)
+                for i, (url, email_list) in enumerate(email_addresses.items(), start=1):
+                    st.write(f"\n{i}. {url}\n")
+                    for email in email_list:
+                        st.write(f"- {email}")
+            elif api_choice == 'Google Search' and google_search_api_key and search_engine_id:
+                st.info("Fetching URLs and e-mails using Google Custom Search API...")
+                urls = get_search_results(search_query, num_results, google_search_api_key, search_engine_id)
+                email_addresses = find_email_addresses(urls)
+                for i, (url, email_list) in enumerate(email_addresses.items(), start=1):
+                    st.write(f"\n{i}. {url}\n")
+                    for email in email_list:
+                        st.write(f"- {email}")
+            else:
+                st.error("Missing API key or search engine ID. Please check the configuration.")
 else:
     if is_user_locked():
         st.error("Too many failed login attempts. Please try again later.")
